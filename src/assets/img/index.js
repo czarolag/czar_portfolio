5ffbfec092fb8941a6fdbd8b7f448339d85a96a3
czarolag <czarolag@gmail.com>
2024-09-17 17:28:41 -0700

    added index.js for exporting assets

---
+++ src/assets/img/index.js
@@ -0,0 +1,21 @@
+// logos
+import headerImg from './header-img.svg';
+import logo from './CZAR-logo.png';
+
+// navigational icons
+import navIcon1 from './nav-icon1.svg';
+import navIcon2 from './nav-icon2.svg';
+import navIcon3 from './nav-icon3.svg';
+
+
+
+export const nav_icons = {
+    navIcon1,
+    navIcon2,
+    navIcon3,
+};
+
+export const logos = {
+    headerImg,
+    logo,
+};
